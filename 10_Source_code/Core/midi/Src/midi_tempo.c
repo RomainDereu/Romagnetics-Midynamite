0f087fd432c88e0e230096c4aed6ee03b495b444
Romain Dereu <rdereu@gmail.com>
2025-09-05 18:19:08 +0900

    Moving midi_tempo to its own logic

--- 10_Source_code/Core/midi/Src/midi_tempo.c
+++ 10_Source_code/Core/midi/Src/midi_tempo.c
@@ -4,5 +4,56 @@
  *  Created on: Sep 5, 2025
  *      Author: Astaa
  */
+#include "menu_tempo.h"
+#include "utils.h"
+#include "midi_usb.h"
+
+void send_midi_tempo_out(int32_t tempo_click_rate, uint8_t send_to_midi_out){
+	uint8_t clock_tick = 0xF8;
+
+	static UART_HandleTypeDef *UART_list_tempo[2];
+	list_of_UART_to_send_to(send_to_midi_out, UART_list_tempo);
 
 
+    send_usb_midi_message(&clock_tick, 1);
+    for (int i = 0; i < 2; i++) {
+        if (UART_list_tempo[i] != NULL) {
+            HAL_UART_Transmit(UART_list_tempo[i], &clock_tick, 1, 1000);
+        	}
+    	}
+	TIM2->ARR = tempo_click_rate;
+    }
+
+void mt_start_stop(TIM_HandleTypeDef *timer) {
+	uint8_t clock_start = 0xFA;
+	uint8_t clock_stop  = 0xfC;
+
+	static UART_HandleTypeDef *UART_list_tempo[2];
+	uint8_t send_out_to = save_get(MIDI_TEMPO_SEND_TO_MIDI_OUT);
+	list_of_UART_to_send_to(send_out_to, UART_list_tempo);
+
+	uint8_t clock_sending = save_get(MIDI_TEMPO_CURRENTLY_SENDING);
+
+    // Stop clock
+    if (clock_sending == 0) {
+        HAL_TIM_Base_Stop_IT(timer);
+
+        for (int i = 0; i < 2; i++) {
+            if (UART_list_tempo[i] != NULL) {
+                HAL_UART_Transmit(UART_list_tempo[i], &clock_stop, 1, 1000);
+            }
+        }
+        send_usb_midi_message(&clock_stop, 1);
+    }
+    // Start clock
+    else if (clock_sending == 1) {
+        for (int i = 0; i < 2; i++) {
+            if (UART_list_tempo[i] != NULL) {
+                HAL_UART_Transmit(UART_list_tempo[i], &clock_start, 1, 1000);
+            }
+        }
+        send_usb_midi_message(&clock_start, 1);
+
+        HAL_TIM_Base_Start_IT(timer);
+    }
+}
